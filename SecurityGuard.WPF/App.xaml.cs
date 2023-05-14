1b2d27e750e83fa3a6d593bc8388daebcae3b6c7
Nikita <pashok.89iq@mail.ru>
2023-05-14 23:19:15 +0300

    registrated my services and setup host

--- SecurityGuard.WPF/App.xaml.cs
+++ SecurityGuard.WPF/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MVVMEssentials.Services;
+using MVVMEssentials.Stores;
+using MVVMEssentials.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +18,42 @@ namespace SecurityGuard.WPF
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host
+                 .CreateDefaultBuilder()
+                 .ConfigureServices((context, serviceCollection) =>
+                 {
+                     serviceCollection.AddSingleton<NavigationStore>();
+                     serviceCollection.AddSingleton<ModalNavigationStore>();
+
+                     serviceCollection.AddSingleton<INavigationService>();
+                     serviceCollection.AddSingleton<CloseModalNavigationService>();
+
+                     serviceCollection.AddSingleton<MainViewModel>();
+                     serviceCollection.AddSingleton<MainWindow>((services) => new MainWindow()
+                     {
+                         DataContext = services.GetService<MainViewModel>()
+                     });
+                 })
+                .Build();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync();
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
     }
 }
