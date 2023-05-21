b68f717a1f64536b66679c99753e0c26b01a94e0
Nikita <pashok.89iq@mail.ru>
2023-05-21 21:42:47 +0300

    add view models for layout nad navbar

---
+++ SecurityGuard.WPF/ViewModels/NavigationBarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVVMEssentials.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.WPF.ViewModels
+{
+    public class NavigationBarViewModel : ViewModelBase
+    {
+
+    }
+}
