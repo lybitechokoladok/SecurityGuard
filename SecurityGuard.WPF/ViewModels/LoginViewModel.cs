a657aa5128cae329a5387af653b2ad097f130266
Nikita <pashok.89iq@mail.ru>
2023-05-21 21:45:59 +0300

    create initial login view model

---
+++ SecurityGuard.WPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,48 @@
+﻿using MVVMEssentials.Services;
+using MVVMEssentials.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SecurityGuard.WPF.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+        }
+    }
+}
