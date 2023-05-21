94368b1ecc75703d4d834faad8c2a480650c9143
Nikita <pashok.89iq@mail.ru>
2023-05-21 21:52:33 +0300

    create layout navigation service

---
+++ SecurityGuard.WPF/Services/LayoutNavigationService.cs
@@ -0,0 +1,32 @@
+﻿using MVVMEssentials.Services;
+using MVVMEssentials.Stores;
+using SecurityGuard.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.WPF.Services
+{
+    public class LayoutNavigationService<TViewModel> : INavigationService where TViewModel : class
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TViewModel> _createViewModel;
+        private readonly Func<NavigationBarViewModel> _createNavigationBarViewModel;
+
+        public LayoutNavigationService(NavigationStore navigationStore,
+            Func<TViewModel> createViewModel,
+            Func<NavigationBarViewModel> createNavigationBarViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+            _createNavigationBarViewModel = createNavigationBarViewModel;
+        }
+
+        public void Navigate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
