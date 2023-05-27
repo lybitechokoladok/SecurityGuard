1456e088a0f3b2cc32a3635c49145862be4f2fba
Nikita <pashok.89iq@mail.ru>
2023-05-27 23:51:55 +0300

    add login command

---
+++ SecurityGuard.WPF/Commands/LoginCommand.cs
@@ -0,0 +1,37 @@
+﻿using MVVMEssentials.Commands;
+using MVVMEssentials.Services;
+using SecurityGuard.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.WPF.Commands
+{
+    public class LoginCommand : AsyncCommandBase
+    {
+        private readonly LoginViewModel _viewModel;
+        private readonly INavigationService _navigationService;
+
+        public LoginCommand(INavigationService navigationService, LoginViewModel viewModel)
+        {
+            _navigationService = navigationService;
+            _viewModel = viewModel;
+
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e) 
+        {
+            if( e.PropertyName == nameof(LoginViewModel.Username) ||
+                e.PropertyName == nameof(LoginViewModel.Password))
+                OnCanExecuteChanged();
+        }
+        protected override async Task ExecuteAsync(object parameter)
+        {
+            _navigationService.Navigate();
+        }
+    }
+}
