d91e67ff319d9644af292c13dd0fb9d6549dbcfe
Nikita <pashok.89iq@mail.ru>
2023-06-04 23:39:45 +0300

    add domain services

---
+++ SecurityGuard.Domain/Services/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using SecurityGuard.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Domain.Services
+{
+    public interface IAccountService
+    {
+        Task<UserDto> LoginAsync(string username, string password);
+        Task<UserDto> RegisterAsunc(string username, string password);
+    }
+}
