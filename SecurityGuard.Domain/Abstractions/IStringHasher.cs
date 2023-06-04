34fa3eff4d54de057e2246c849e7fbb2bbf2b972
Nikita <pashok.89iq@mail.ru>
2023-06-04 23:51:08 +0300

    add password hash interface

---
+++ SecurityGuard.Domain/Abstractions/IStringHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Domain.Abstractions
+{
+    public interface IStringHasher
+    {
+        string Hash(string password);
+        bool HashesMatch(string hash, string providedPassword);
+    }
+}
