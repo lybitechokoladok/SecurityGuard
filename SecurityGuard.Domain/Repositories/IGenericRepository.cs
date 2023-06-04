2fff8c1b363b249ee0011d77b7261b72bceb1d64
Nikita <pashok.89iq@mail.ru>
2023-06-04 23:25:27 +0300

    add generic repository interface

---
+++ SecurityGuard.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Domain.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllListAsync();
+        Task AddAsync(T entity);
+        Task RemoveAsync(int id);
+        Task UpdateAsync(T entity);
+    }
+}
