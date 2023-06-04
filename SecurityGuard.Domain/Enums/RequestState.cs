4bdc4eae9021cf5f9645f3205a85e6f763b3f5bc
Nikita <pashok.89iq@mail.ru>
2023-06-04 23:41:48 +0300

    add domain enum for request states

---
+++ SecurityGuard.Domain/Enums/RequestState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Domain.Enums
+{
+    public enum RequestState
+    {
+        New = 0,
+        Approved = 1,
+        InProcess = 2,
+        Closed = 3
+    }
+}
