3e9d654a8ca699bf47c6e0ef03e85fd72ad40488
Nikita <pashok.89iq@mail.ru>
2023-06-05 22:13:41 +0300

    add DBContext

---
+++ SecurityGuard.Infrastructure/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Infrastructure
+{
+    public class DBContext
+    {
+        private readonly IConfiguration _configuration;
+        public DBContext(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        public SqlConnection CreateConnection() 
+        {
+            return new SqlConnection(_configuration.GetConnectionString("default"));
+        }
+    }
+}
