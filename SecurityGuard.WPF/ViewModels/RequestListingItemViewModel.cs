ab0f70e9f3062af0a734976ba273e98ffccb37b7
lybitechokoladok <102763296+lybitechokoladok@users.noreply.github.com>
2023-06-11 23:53:42 +0300

    add request listing item view model

---
+++ SecurityGuard.WPF/ViewModels/RequestListingItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.WPF.ViewModels
+{
+    public class RequestListingItemViewModel
+    {
+        public int RequestNumber { get; set; }
+
+        public string FullName { get; set; }
+
+
+        public DateTime ArrivalDate { get; set; }
+
+        public RequestListingItemViewModel(int requestNumber, string fullName, DateTime arrivalDate)
+        {
+            RequestNumber = requestNumber;
+            FullName = fullName;
+            ArrivalDate = arrivalDate;
+        }
+    }
