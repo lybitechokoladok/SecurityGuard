0636ea12f007dfdb7081e7af8a66f40b8f26840c
lybitechokoladok <102763296+lybitechokoladok@users.noreply.github.com>
2023-06-14 23:28:21 +0300

    add ModalNavigationService

---
+++ SecurityGuard.WPF/Services/ModalNavigationService.cs
@@ -0,0 +1,29 @@
+﻿using MVVMEssentials.Services;
+using MVVMEssentials.Stores;
+using MVVMEssentials.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.WPF.Services
+{
+    public class ModalNavigationService<TViewModel> : INavigationService
+        where TViewModel : ViewModelBase
+    {
+        private readonly ModalNavigationStore _navigationStore;
+        private readonly Func<TViewModel> _createViewModel;
+
+        public ModalNavigationService(ModalNavigationStore navigationStore, Func<TViewModel> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
+}
