9545bba413fecce667578181d4b6fffb594393c1
Nikita <pashok.89iq@mail.ru>
2023-06-19 11:10:52 +0300

    add GroupMember repository

---
+++ SecurityGuard.Domain/Repositories/IGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿using SecurityGuard.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.Domain.Repositories
+{
+    public interface IGroupRepository
+    {
+        Task<MembersGroup> GetGroupByIdAsync(int groupId);
+        Task<IEnumerable<GroupMember>> GetAllGroupMemberListAsync(int groupId);
+    }
+}
